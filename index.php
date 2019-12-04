b0f64f9dc722ee7ba666b5966f6fb5fa8ef3a5f4
PaulineGrn <pauline.gernigon@orange.fr>
2019-12-04 14:27:13 +0100

    PHP2Ex3

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <!-- Déclaration d'une variable gender -->
 <?php
-$gender='homme';
+$gender='femme';
 ?>
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
@@ -9,17 +9,11 @@ $gender='homme';
   <title>PHP2Ex3</title>
 </head>
 <body>
-  <?php
-  // Si c'est une femme alors c'est une développeuse
-  if($gender=='femme')
-  {
-    echo'C\'est une développeuse !!!';
-  }
-  // Sinon c'est un développeur
-  else
-  {
-    echo'C\'est un développeur !!!';
-  }
-  ?>
-</body>
-</html>
+  <!-- Si c'est une femme alors c'est une développeuse -->
+  <?php if($gender=='femme'){ ?>
+    <p>C\'est une développeuse !!!</p>
+    <?php} else{ ?>
+      <p>C\'est un développeur !!!</p>
+    <?php } ?>
+  </body>
+  </html>
